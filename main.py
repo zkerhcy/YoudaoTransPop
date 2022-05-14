eb7347155a38b9da7692b23c1e83a73583a8cee3
chenzhao <czhao5956@gmail.com>
2022-05-14 13:20:59 +0800

    dev for multiple line

--- main.py
+++ main.py
@@ -17,22 +17,34 @@ KEY_CODE = {
     "SEC_KEY": "EClQZIFpNavmg5Zpx7XRgJk9bHz0fwC5"
 }
 
+LINE_CHAR = 20
+
 import os
 import ydtrans
 import json
 
 try:
-    translator = ydtrans.Translator(app_key=KEY_CODE['APP_KEY'],
-                                    text=os.environ['POPCLIP_TEXT'],
-                                    sec_key=KEY_CODE['SEC_KEY'])
+    translator = ydtrans.Translator(
+        app_key=KEY_CODE['APP_KEY'],
+        text=os.environ['POPCLIP_TEXT'],
+        sec_key=KEY_CODE['SEC_KEY'])
 
-    translation = translator.translate_text(text=os.environ['POPCLIP_TEXT'],
-                                            from_lang='auto',
-                                            to_lang='auto',
-                                            app_key=KEY_CODE['APP_KEY'])
+    translation = translator.translate_text(
+        text=os.environ['POPCLIP_TEXT'],
+        from_lang='auto',
+        to_lang='auto',
+        app_key=KEY_CODE['APP_KEY'])
 
     # s = json.loads(json.dumps(translation,ensure_ascii=False))
-    print translation["translation"][0].encode('utf-8')
+    txt = translation["translation"][0]
+    # line = len(txt) / LINE_CHAR + 1
+    # if line > 1:
+    #     for i in range(line):
+    #         if i == 0:
+    #             txt = txt[:(LINE_CHAR - 1)] + '\r\n' + txt[(LINE_CHAR - 1):]
+    #         else:
+    #             txt = txt[:(LINE_CHAR * (i + 1))] + '\r\n' + txt[(LINE_CHAR * (i + 1)):]
+    print txt.encode('utf-8')
 
 except Exception as e:
     exit(1)
--- test.py
+++ test.py
@@ -6,6 +6,8 @@ KEY_CODE = {
     "SEC_KEY": "EClQZIFpNavmg5Zpx7XRgJk9bHz0fwC5"
 }
 
+LINE_CHAR = 20
+
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(dest='text', type=str, default='hello', help='a string to translate')
 parser.add_argument('-t', '--to', dest='to_lang', type=str,
@@ -14,13 +16,23 @@ parser.add_argument('-f', '--from', dest='from_lang',
                     type=str, default='auto', help='from language')
 args = parser.parse_args()
 
-translator = ydtrans.Translator(app_key=KEY_CODE['APP_KEY'],
-                                text=args.text,
-                                sec_key=KEY_CODE['SEC_KEY'])
+translator = ydtrans.Translator(
+    app_key=KEY_CODE['APP_KEY'],
+    text=args.text,
+    sec_key=KEY_CODE['SEC_KEY'])
 
-translation = translator.translate_text(text=args.text,
-                                        from_lang=args.from_lang,
-                                        to_lang=args.to_lang,
-                                        app_key=KEY_CODE['APP_KEY'])
+translation = translator.translate_text(
+    text=args.text,
+    from_lang=args.from_lang,
+    to_lang=args.to_lang,
+    app_key=KEY_CODE['APP_KEY'])
 
-print translation["translation"][0].encode('utf-8')
+txt = translation["translation"][0]
+line = len(txt) / LINE_CHAR + 1
+if line > 1:
+    for i in range(line):
+        if i == 0:
+            txt = txt[:(LINE_CHAR - 1)] + '\r\n' + txt[(LINE_CHAR - 1):]
+        else:
+            txt = txt[:(LINE_CHAR * (i + 1))] + '\r\n' + txt[(LINE_CHAR * (i + 1)):]
+print txt.encode('utf-8')
